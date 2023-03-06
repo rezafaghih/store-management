2df785d2d27484004923590ae853a0e3d548e4c3
Rezafaghih <royalgaming.work@gmail.com>
2023-03-06 16:50:13 +0330

    add button and close button created

--- src/javascript/global.js
+++ src/javascript/global.js
@@ -3,11 +3,41 @@ window.addEventListener("load", function(){
   if (hidde_container != undefined){
     var button = this.document.querySelector("#upload-new-product");
     var visible = false;
-
+    var close_button = this.document.querySelector("#close_button");
     button.addEventListener("click", function(){
       if (visible == false){
         hidde_container.style.display = "flex";
+        visible = true;
       }
+    })
+
+    close_button.addEventListener("click", function(){
+      if (visible){
+        hidde_container.style.display = "none";
+        visible = false;
+      }
+    })
+  }
+
+  var add = this.document.querySelectorAll("#more-count");
+  var less = this.document.querySelectorAll("#less-count");
+
+  var count_of_product_span = this.document.querySelectorAll("#count-of-product");
+
+
+  for (let index = 0; index < add.length; index++) {
+    add[index].addEventListener("click", function(){
+      value = parseInt(count_of_product_span[index].innerHTML);
+      count_of_product_span[index].innerHTML = value+1;
+      value = null;
+    })
+  }
+
+  for (let index = 0; index < less.length; index++) {  
+    less[index].addEventListener("click", function(){
+      value = parseInt(count_of_product_span[index].innerHTML);
+      count_of_product_span[index].innerHTML = value-1;
+      value = null;
     })
   }
 })
